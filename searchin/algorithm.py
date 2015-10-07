2b24ae807b256e1bb18b2a994516696b02473c34
whypro <whypro@live.cn>
2015-10-07 12:26:30 +0800

    Fix bug

--- searchin/algorithm.py
+++ searchin/algorithm.py
@@ -30,37 +30,4 @@ def calculate_relevancy_paper(year, cite_num, click_num):
     relevancy_paper = a * 0.5 + b * 0.3 + c * 0.2
 
     return relevancy_paper
-    
-#用于计算图书的排名影响因子  注意后缀 "_book" 的区别
-def calculate_relevancy_book(year, query_ratio, click_num):
-    if not year:
-        return 0
-        
-    #第一步：
-    #元素说明与计算
-    #考虑三个影响因子 ： 出版时间（year）、query占比(query_ratio)、点击量(click_num)
-    
-    #关于query占比影响的计算：（最好在查询前已经线下计算好了每一本书名字有多少个字）
-    query_ratio = query_ratio(比如“推荐”)/title_num（比如：“推荐系统”） = 2.0/4 = 0.5
-    
-    #关于出版时间影响的计算：
-    year_factor = 1.0/(datetime.date.today().year - year + YEAR_PARAM )  ##和论文的出版时间计算一样
-    
-    #关于点击量影响的计算：
-    #首先声明：点击量每次获取必须满足 query_ratio > 0 
-    click_factor = log(click_num ) / log(max_click_num )  ##引入log是为了缩小点击量之间的落差
-    
-    #第二步：
-    #影响因子归一化，防止某一个参数过大，对其他参数形成消弱
-    sum_factor = query_ratio + year_factor + click_factor
-    query_ratio = query_ratio / sum_factor
-    year_factor = year_factor / sum_factor
-    click_factor = click_factor / sum_factor    
-    
-    #第三步：
-    #综合计算相关度
-    relevancy_book = query_ratio * 0.5 + year_factor * 0.25 + click_factor * 0.25
-    
-    #第四步：
-    #返回相关度
-    return relevancy_book
+
